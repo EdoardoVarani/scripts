4c02ed982c170e8f6935220a286d148cfa284df5
edoardo.varani <edoardo.varani>
2016-10-14 12:49:35 +0200

    v0.1

---
+++ dbIOscript.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+MYSQLHOST="127.0.0.1"
+MYSQLDB="testdb"
+MYSQLUSER="testuser"
+MYSQLPASS="pass"
+ 
+#File name with date only
+DATE=$(date + "%F")
+ 
+#File location
+FILE="/tmp/Usage_Report_$DATE.csv"
+ 
+MYSQLOPTS="--password=${MYSQLPASS}"
+ 
+#testing purposes, give echo output
+echo "Report Begin: $(date)"
+
+ 
+mysql ${MYSQLOPTS} << EOFMYSQL
+USE sys
+select table_name, io_read, io_write from schema_table_statistics where table_schema="employees" order by io_read, io_write;
+INTO OUTFILE '$FILE' FIELDS TERMINATED BY ','
+LINES TERMINATED BY '\n';
+EOFMYSQL
+ 
+#add column title to the report
+sed -i '1i User_Name,User_ID' $FILE
